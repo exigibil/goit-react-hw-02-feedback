d4e2be2944b4c93bdf26be5883038a00beb8a18d
exigibil <adrian.topa04@gmail.com>
2024-04-16 19:11:58 +0300

    Update Feedback.jsx

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,38 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import styles from './feedback.module.css';
 
-function Feedback({ message }) {
+function Feedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const totalFeedback = good + neutral + bad;
+  const positivePercentrage = totalFeedback > 0 ? ((good / totalFeedback) * 100).toFixed(2) : 0;
+
+
+  const updateCount = (setCount) => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
   return (
-    <>
-    
-
-      <div className={styles.feedbackContainer}>
-      
-            <h1 className={styles.feedbackHeader} >Topa React</h1>
-       
-        <h2> Please leave your {message}</h2>
-        <div className={styles.feedbackWrapper}>
-          <button className={styles.feedbackMood}>Good</button>
-          <button className={styles.feedbackMood}>Neutral</button>
-          <button className={styles.feedbackMood}>Bad</button>
-        </div>
-
-        <div>
-          <h2>Statisctics</h2>
-          <p>Good:</p>
-          <p>Neutral:</p>
-          <p>Bad:</p>
-          <p>Total:</p>
-          <p>Positive:</p>
-        </div>
+    <div className={styles.feedbackContainer}>
+      <h1 className={styles.feedbackHeader}>Topa's React Feedback</h1>
+      <h2>Please leave your feedback</h2>
+      <div className={styles.feedbackWrapper}>
+        <button onClick={() => updateCount(setGood)} className={styles.feedbackMood}>Good</button>
+        <button onClick={() => updateCount(setNeutral)} className={styles.feedbackMood}>Neutral</button>
+        <button onClick={() => updateCount(setBad)} className={styles.feedbackMood}>Bad</button>
       </div>
-    </>
+
+      <div>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {totalFeedback}</p>
+        <p>Positive: {positivePercentrage}%</p>
+      </div>
+    </div>
   );
 }
-
-Feedback.propsTypes = {
-  message: PropTypes.string.isRequired,
-};
 
 export default Feedback;
